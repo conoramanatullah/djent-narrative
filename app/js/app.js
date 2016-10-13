936ee74ab378e2aa55b72258df5bd93459077dce
conoramanatullah <conor.amanatullah@gmail.com>
2016-10-13 02:53:54 -0600

    Final Commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,38 +1,59 @@
 (function(){
   angular.module('Djent', ['ngMaterial'])
   //
-  .controller('MainController', function($scope){
+  .controller('MainController', function($scope, $mdMedia){
     //
+    $scope.content = false;
     $scope.pageIndex = 1;
     $scope.start = "start";
-    //
+    $scope.text = "djent";
+    $scope.dval = 0;
     $scope.startDjent = function() {
       // Get total number of pages
-      if($scope.pageIndex <= 6){
-        if ($scope.pageIndex == 6){
+      if($scope.pageIndex <= 5){
+        if ($scope.pageIndex == 1){
+          $scope.content = true;
+        }
+        else if ($scope.pageIndex == 5){
           $scope.pageIndex++;
           $scope.arrow = false;
-          $scope.start = "Finished";
-          console.log("Done");
+          $scope.start = "end";
           return;
         }
         $scope.arrow = true;
         $scope.start = "next";
         $scope.pageIndex++;
-        console.log("lets djent " + $scope.pageIndex);
       } else {
 
       }
 
     };
+    $scope.inc = function(ev){
+      $scope.dval++;
+      if($scope.dval == 5){
+        $scope.text = "0000000";
+      }
+    }
     //
+  })
+  .controller('pageTwoController', function($scope, $mdMedia, $mdDialog){
+
+  })
+  .controller('pageThreeController', function($scope, $mdMedia, $mdDialog){
+
+  })
+  .controller('pageFourController', function($scope, $mdMedia, $mdDialog){
+
+  })
+  .controller('pageFiveController', function($scope, $mdMedia, $mdDialog){
+
   })
   //
   .directive('openTools', function() {
     return {
       link: function(scope, elem, attrs) {
         elem.on('click',function(){
-          const pos = elem.css('bottom');
+          var pos = elem.css('bottom');
           if(pos == '0px'){
             elem.css('bottom','-15vh');
           } else {
